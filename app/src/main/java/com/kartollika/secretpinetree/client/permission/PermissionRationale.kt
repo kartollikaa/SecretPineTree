f4e9728e44476a5d63a65ef9ed9940b55830e857
Dmitrii Maksimov <dmitrij.maksimow@gmail.com>
2022-11-20 13:04:07 +0300

    My idea broke

--- app/src/main/java/com/kartollika/secretpinetree/client/permission/PermissionRationale.kt
+++ app/src/main/java/com/kartollika/secretpinetree/client/permission/PermissionRationale.kt
@@ -1,5 +1,8 @@
 package com.kartollika.secretpinetree.client.permission
 
+import android.content.Intent
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.result.ActivityResult
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +14,7 @@ import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -24,6 +28,7 @@ import com.kartollika.secretpinetree.client.R.string
 @Composable
 fun PermissionRationale(
   permissionLauncher: MultiplePermissionsState,
+  locationEnabled: () -> Boolean,
   modifier: Modifier = Modifier
 ) {
   ProvideTextStyle(value = LocalTextStyle.current.copy(textAlign = TextAlign.Center)) {
@@ -39,6 +44,40 @@ fun PermissionRationale(
       Button(onClick = { permissionLauncher.launchMultiplePermissionRequest() }) {
         Text(text = stringResource(string.permissions_rationale_give_permissions))
       }
+    }
+  }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+private fun actionIfEnabledLocation(
+  state: MutableState<(() -> Unit)?>,
+  permissionsState: MultiplePermissionsState,
+  locationEnabled: () -> Boolean,
+  locationEnableLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>,
+  action: () -> Unit
+) {
+  when {
+    !permissionsState.allPermissionsGranted -> {
+      permissionsState.launchMultiplePermissionRequest()
+      state.value = {
+        actionIfEnabledLocation(
+          state, permissionsState, locationEnabled, locationEnableLauncher, action
+        )
+      }
+    }
+
+    !locationEnabled() -> {
+      locationEnableLauncher.launch(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+      state.value = {
+        actionIfEnabledLocation(
+          state, permissionsState, locationEnabled, locationEnableLauncher, action
+        )
+      }
+    }
+
+    else -> {
+      action()
+      state.value = null
     }
   }
 }
